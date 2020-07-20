0c20fcb89c36b075d4a9484e4187235cbddce0de
Kingsleymuturi <kingsleymuturi9@gmail.com>
2020-07-20 12:01:25 +0300

    update profile

--- app/models.py
+++ app/models.py
@@ -12,7 +12,7 @@ class User (UserMixin,db.Model):
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255),unique = True,nullable = False)
     email = db.Column(db.String(255), unique = True,nullable = False)
-    bio = db.Column(db.String(255),default ='My default Bio')
+    bio = db.Column(db.String(255),default ='Default bio')
     profile_pic_path = db.Column(db.String(150),default ='default.png')
     hashed_password = db.Column(db.String(255),nullable = False)
     blog = db.relationship('Blog', backref='user', lazy='dynamic')
