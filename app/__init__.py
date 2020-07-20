961d08b73ce3e5c27db57b277b4b4d53bb0df063
Kingsleymuturi <kingsleymuturi9@gmail.com>
2020-07-20 13:59:35 +0300

    update all

--- app/__init__.py
+++ app/__init__.py
@@ -16,9 +16,10 @@ photos = UploadSet('photos',IMAGES)
 
 
 
-def create_app():
+def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(Config)
+    app.config.from_object(config_options[config_name])
     from .auth import auth as authentication_blueprint
     from .main import main as main_blueprint
 
