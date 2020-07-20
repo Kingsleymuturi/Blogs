451f8a243b2b027ea34dfc36fe68d0597adb5e64
Kingsleymuturi <kingsleymuturi9@gmail.com>
2020-07-20 14:03:27 +0300

    deployment to heroku

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask 
 from config import Config
+from config import config_options
 from flask_mail import Mail
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
